24e233e33fc929dc9c8e1b7ad3060c6c8f774c8f
Muhammad Fawad Rafique <fawadrafique@hotmail.com>
2020-08-19 10:48:17 +0200

    Update conditional statement

--- js/widget.js
+++ js/widget.js
@@ -7,16 +7,17 @@ let i = 0,
 //del = {};
 
 function addWidget(data, cityAndCountry) {
-    i++
     icon = getIcon(data, 'noTag')
 
     if (i <= 2) {
+        i++
         console.log(i)
         document.querySelector(`#city${i}`).textContent = cityAndCountry
         document.querySelector(`#temp${i}`).innerHTML = `<span class="text-xs">L </span><span class="text-sm">${Math.round(data.daily[0].temp.min)}º - </span><span class="text-xs">H</span>
               <span class="text-sm">${Math.round(data.daily[0].temp.max)}º</span>`
         document.querySelector(`#icon${i}`).innerHTML = `<i class="${icon} text-3xl"></i>`
-    } else i = 0;
+        if (i == 2) i = 0;
+    }
 
     //     del[`del${i}`] = document.querySelector(`#del${i}`);
     //     console.log(del)
