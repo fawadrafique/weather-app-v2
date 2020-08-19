75eb3bd624cdcd6966a57cb446f4d8da6e733adc
Muhammad Fawad Rafique <fawadrafique@hotmail.com>
2020-08-19 10:31:16 +0200

    Add js handle fetch callback function module

---
+++ js/handlefetch.js
@@ -0,0 +1,21 @@
+import getDay from './updateday.js';
+import weather from './currentweather.js';
+import updateBackground from './background.js';
+import updateForecast from './forecast.js';
+import updateChart from './updatechart.js';
+import addWidget from './widget.js';
+import {
+    cityAndCountry
+} from './index.js';
+
+
+
+function handleFetch(data) {
+    weather(data);
+    getDay();
+    addWidget(data, cityAndCountry)
+    updateForecast(data);
+    updateBackground(city);
+    updateChart(data);
+}
+export default handleFetch;
